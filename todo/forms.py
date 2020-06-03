02ee98a50a0c94fc72fbb76946268d6098b91b4d
howakuro <tkok2568@gmail.com>
2020-06-04 06:39:40 +0900

    UPDATE 登録に必要なデータをパスワードとユーザー名のみに変更。

--- todo/forms.py
+++ todo/forms.py
@@ -53,9 +53,8 @@ class SignUpForm(UserCreationForm):
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = field.label
         self.fields['username'].label = 'Username'
-        self.fields['email'].label = 'E-Mail'
         self.fields['password1'].label = 'Password'
         self.fields['password2'].label = 'Password（確認用）'
     class Meta:
         model = ToDoUser
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'password1', 'password2')
