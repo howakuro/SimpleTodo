e7cc047dfdda9190d9de96f2405f89209bad2cdb
howakuro <tkok2568@gmail.com>
2020-06-07 18:11:06 +0900

    UPDATE 各種STATICファイルをroot化。

--- simpletodo/settings/settings.py
+++ simpletodo/settings/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(os.getcwd(), "static")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'todo/static/todo/'),
+    os.path.join(os.getcwd(), "todo", "static", "todo"),
 )
 
 #Login
